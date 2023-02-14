5d1f1d78b7603b5c7ce48b861d4b27b0140c077d
lyjeileen <lyjeileen@gmail.com>
2023-02-13 16:17:24 -0800

    Add Link to redirect to product page

--- components/ProductCard.tsx
+++ components/ProductCard.tsx
@@ -1,4 +1,5 @@
 import Image from 'next/image';
+import Link from 'next/link';
 
 import { ProductItem } from './ProductGrid';
 
@@ -12,28 +13,30 @@ export const ProductCard = (props: ProductCardProps) => {
 
   return (
     <div key={id} className="aspect-squre mx-4 mb-2">
-      <div className="relative aspect-[6/5]">
-        {images[0] && (
-          <Image
-            alt={title}
-            src={images[0]?.originalSrc}
-            fill
-            sizes="(max-width: 640px) 100vw,
+      <Link href={`product/${product.id}`}>
+        <div className="relative aspect-[6/5]">
+          {images[0] && (
+            <Image
+              alt={title}
+              src={images[0]?.originalSrc}
+              fill
+              sizes="(max-width: 640px) 100vw,
               (max-width: 768px) 50vw,
               33vw"
-            className="border rounded-lg"
-          />
-        )}
-      </div>
+              className="border rounded-lg"
+            />
+          )}
+        </div>
 
-      <div className="flex m-2">
-        <div className="ml-2">
-          <p className="font-semibold">{title}</p>
-          <div className="text-xs text-gray-700">
-            <div>${price}</div>
+        <div className="flex m-2">
+          <div className="ml-2">
+            <p className="font-semibold">{title}</p>
+            <div className="text-xs text-gray-700">
+              <div>${price}</div>
+            </div>
           </div>
         </div>
-      </div>
+      </Link>
     </div>
   );
 };
